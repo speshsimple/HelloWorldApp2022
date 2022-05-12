565e68fdcaba6c04262846b3e47d6a743a8ef6cc
Oleksandr Novikov <speshsimple@me.com>
2022-05-12 23:26:07 +0200

    Initial Commit

---
+++ HelloWorldApp2022/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorldApp2022
+//
+//  Created by Alex Novikov on 10.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
