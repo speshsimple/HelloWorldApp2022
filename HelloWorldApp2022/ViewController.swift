b9f363e58742054f5304f153e6c9344286d401b9
Oleksandr Novikov <speshsimple@me.com>
2022-05-16 01:01:39 +0200

    second view and stack view

--- HelloWorldApp2022/ViewController.swift
+++ HelloWorldApp2022/ViewController.swift
@@ -14,12 +14,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        helloWorldLabel.isHidden = true
+        showTextButton.layer.cornerRadius = 10
     }
 
     
     @IBAction func showTextPressed() {
+        if helloWorldLabel.isHidden {
+            helloWorldLabel.isHidden = false
+            showTextButton.setTitle("Clear Text", for: .normal)
+        } else {
+            helloWorldLabel.isHidden = true
+            showTextButton.setTitle("Show Text", for: .normal)
+        }
     }
-    
 }
 
